b1d0df22c900238106d83919a13b2d93f8d4015e
Daniel Brewer <brewerdan@me.com>
2014-03-14 23:24:34 +0000

    Model changes and 3d-real-time-plotting.

--- src/MagPlot.py
+++ src/MagPlot.py
@@ -1,25 +1,27 @@
 import numpy as np
 import time
+import matplotlib.pyplot as mplpp
 from matplotlib.pylab import subplots,close
 from matplotlib import cm
 
-matplotlib.use('TkAgg')
+mplpp.switch_backend('TkAgg')  
 
-def randomwalk(dims=(256,256),n=20,sigma=5,alpha=0.95,seed=1):
+def randomwalk(dims=(256,256,256),n=1,sigma=5,alpha=0.95,seed=1):
     """ A simple random walk with memory """
 
-    r,c = dims
+    r,c,d = dims
     gen = np.random.RandomState(seed)
-    pos = gen.rand(2,n)*((r,),(c,))
-    old_delta = gen.randn(2,n)*sigma
+    pos = gen.rand(3,n)*((r,),(c,),(d,))
+    old_delta = gen.randn(3,n)*sigma
 
     while 1:
 
-        delta = (1.-alpha)*gen.randn(2,n)*sigma + alpha*old_delta
+        delta = (1.-alpha)*gen.randn(3,n)*sigma + alpha*old_delta
         pos += delta
         for ii in xrange(n):
             if not (0. <= pos[0,ii] < r) : pos[0,ii] = abs(pos[0,ii] % r)
             if not (0. <= pos[1,ii] < c) : pos[1,ii] = abs(pos[1,ii] % c)
+            if not (0. <= pos[2,ii] < d) : pos[2,ii] = abs(pos[2,ii] % d)
         old_delta = delta
         yield pos
 
@@ -35,39 +37,31 @@ def run(niter=1000,doblit=True):
     ax.set_ylim(0,255)
     ax.hold(True)
     rw = randomwalk()
-    x,y = rw.next()
+    x,y,z = rw.next()
     fig.canvas.draw()
 
-    if doblit:
-
-        # cache the background
-        background = fig.canvas.copy_from_bbox(ax.bbox)
-
+    # cache the background
+    background = fig.canvas.copy_from_bbox(ax.bbox)
+    
     plt = ax.plot(x,y,'o')[0]
+    #Axes3D.scatter
     tic = time.time()
 
     for ii in xrange(niter):
 
         # update the xy data
-        x,y = rw.next()
+        x,y,z = rw.next()
+        #Axes3D.scatter
         plt.set_data(x,y)
-
-        if doblit:
-
-            # restore background
-            fig.canvas.restore_region(background)
-
-            # redraw just the points
-            ax.draw_artist(plt)
-
-            # fill in the axes rectangle
-            fig.canvas.blit(ax.bbox)
-
-        else:
-
-            # redraw everything
-            fig.canvas.draw()
-
+        
+        # restore background
+        fig.canvas.restore_region(background)
+        
+        # redraw just the points
+        ax.draw_artist(plt)
+        
+        # fill in the axes rectangle
+        fig.canvas.blit(ax.bbox)
+        
     close(fig)
-    print "Blit = %s, average FPS: %.2f" %(
-           str(doblit),niter/(time.time()-tic))
+    print("Average FPS: %.2f" %(niter/(time.time()-tic)))

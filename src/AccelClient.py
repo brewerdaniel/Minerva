6cff38d0b730981567f4a52ccb55faca4718bac6
Daniel Brewer <brewerdan@me.com>
2014-03-18 15:58:46 +0000

    Client test file.

--- src/AccelClient.py
+++ src/AccelClient.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python           # This is client.py file
 
 import socket               # Import socket module
+import struct
 
 s = socket.socket()         # Create a socket object
-host = '10.0.1.2'#socket.gethostname() # Get local machine name
+host = 'djb231.quns.cam.ac.uk'#socket.gethostname() # Get local machine name
 port = 12345                # Reserve a port for your service.
 
 s.connect((host, port))
-print s.recv(1024)
+data=s.recv(1024)
 s.close                     # Close the socket when done
+
+print struct.unpack('f'*(len(data)/4), data)

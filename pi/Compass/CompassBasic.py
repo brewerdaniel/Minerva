be6d36b788bbce6fc1e3d52f8195e11d06aa53fa
Daniel Brewer <djb231@cam.ac.uk>
2014-12-27 11:58:08 +0000

    This calibration code is rubbish.

---
+++ pi/Compass/CompassBasic.py
@@ -0,0 +1,45 @@
+
+# Initialise the XLoBorg library
+XLoBorg.printFunction = XLoBorg.NoPrint
+XLoBorg.Init()
+
+scale=0.98
+
+x_offset = -618.954
+y_offset = 733.05
+
+
+#Calibration - obtaining hard iron offsets
+def calibrate():
+    global x_offset
+    global y_offset
+    x_sum=0
+    y_sum=0
+    for i in range(0,300):
+        data = XLoBorg.ReadCompassRaw()
+        x_sum += data[0]
+        y_sum += data[1]
+        time.sleep(0.1)
+
+    x_offset = x_sum/300.0
+    y_offset = y_sum/300.0
+
+#calibrate()
+#print "Calibration complete. Sleeping for 10, then taking measurements"
+#time.sleep(10)
+
+for i in range(0,1000):
+    data = XLoBorg.ReadCompassRaw()
+    x_out = data[0] - x_offset
+    y_out = data[1] - y_offset
+    z_out = data[2]
+    
+    normal_x = x_out/_np.sqrt(x_out**2 + y_out**2)
+    normal_y = y_out/_np.sqrt(x_out**2 + y_out**2)
+
+    bearing  = math.atan2(normal_y, normal_x) 
+    if (bearing < 0):
+        bearing += 2 * math.pi
+        
+    print "{0:.8f} {1:.8f} {2:.8f} {3:.8f} {4:.1f}".format(normal_x, normal_y, (data[0]), (data[1]), math.degrees(bearing))
+    time.sleep(0.01)
--- pi/Compass/Sensors.py
+++ pi/Compass/Sensors.py
@@ -21,68 +21,55 @@ XLoBorg.Init()
 #            print "Whoops!"
 
 def heading() :
-    A = XLoBorg.ReadAccelerometer()
-    M = XLoBorg.ReadCompassRaw()
-    
     g = 9.81
     
-        # hard iron estimate
-    Vx, Vy, Vz = (-618, 700.00, 1427.00)
-    
-        # tilt-compensated e-Compass code
-    Ax, Ay, Az = A
-    Mx, My, Mz = M
+    Ax, Ay, Az = XLoBorg.ReadAccelerometer()
     Ax *= g
     Ay *= g
     Az *= g
     
-        # subtract the hard iron offset
-    Mx -= Vx    # see Eq 16
-    My -= Vy    # see Eq 16
-    Mz -= Vz    # see Eq 16
-    
-        # calculate current roll angle
-    roll = math.atan2(Ay, Az)    # Eq 13
+    roll = math.atan2(Ay, Az)
     roll_d = 180*roll/math.pi
-    #print roll_d
+
+    sin_roll = math.sin(roll)
+    cos_roll = math.cos(roll)
+
+    Mx, My, Mz = XLoBorg.ReadCompassRaw()
+    Vx, Vy, Vz = (-618, 700.00, 1427.00) # hard iron estimate
+    Mx -= Vx
+    My -= Vy
+    Mz -= Vz
     
-        # calculate sin and cosine of roll angle Phi
-    sin_roll = math.sin(roll)    # Eq 13: sin = opposite / hypotenuse
-    cos_roll = math.cos(roll)    # Eq 13: cos = adjacent / hypotenuse
-    
-        # de-rotate by roll angle Phi
     By = ((My * cos_roll - Mz * sin_roll))    # Eq 19 y component
     Mz = ((My * sin_roll + Mz * cos_roll))    # Bpy*sin(Phi)+Bpz*cos(Phi)
-    Az = ((My * sin_roll + Mz * cos_roll))    # Eq 15 denominator
+    Az = ((Ay * sin_roll + Az * cos_roll))    # Eq 15 denominator
     
-        # calculate current pitch angle Theta
-    pitch = math.atan2(-Ax, Az)    # Eq 15
+    pitch = math.atan2(-Ax, Az)
     pitch_d = 180*pitch/math.pi
-    #print pitch_d
-    
-        # restrict pitch angle to range -90 to 90 degrees
+
+    print "{0:.4f} {1:.4f}".format(roll_d, pitch_d)
+
     if (pitch > math.pi/2) :
-        pitch = (math.pi - pitch_d)
+        pitch = (math.pi - pitch)
     if (pitch < -math.pi/2) :
-        pitch = (-math.pi - pitch_d)
+        pitch = (-math.pi - pitch)
         
-        # calculate sin and cosine of pitch angle Theta
-    sin_pitch = math.sin(pitch)    # Eq 15: sin = opposite / hypotenuse
-    cos_pitch = math.cos(pitch)    # Eq 15: cos = adjacent / hypotenuse
+    sin_pitch = math.sin(pitch)
+    cos_pitch = math.cos(pitch)
     
-        # correct cosine if pitch not in range -90 to 90 degrees
     if (cos_pitch < 0) :
         cos_pitch = -cos_pitch
     
-        # de-rotate by pitch angle Theta
-    Bx = (Mx * cos_pitch + Mz * sin_pitch)    # Eq 19: x component
-    Bz = (-Mx * sin_pitch + Mz * cos_pitch)    # Eq 19: z component
+    # de-rotate by pitch angle Theta
+    Bx = (Mx * cos_pitch + My * sin_pitch * sin_roll + Mz * sin_pitch * cos_roll)    # Eq 19: x component
+    Bz = (-Mx * sin_pitch + My * cos_pitch * sin_roll + Mz * cos_pitch * cos_roll)    # Eq 19: z component
     
     nBx = Bx/math.sqrt(Bx**2+By**2)
     nBy = By/math.sqrt(Bx**2+By**2)
-        # calculate current yaw = e-compass angle Psi
-    yaw = math.atan2(-nBy, nBx)  # Eq 22
-    yaw_d = 180*yaw/math.pi
+    
+    # calculate current yaw = e-compass angle Psi
+    bearing = math.atan2(-nBy, nBx)  # Eq 22
+    yaw_d = 180*bearing/math.pi
     print "{0:.4f} {1:.4f} {2:.4f} {3:.4f} {4:.1f}".format(-nBy, nBx, -By, Bx, yaw_d)
 
 #print yaw_d

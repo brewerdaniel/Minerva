67cd13495bfe38ad2ed4b622b43027179cc3c996
Daniel Brewer <djb231@cam.ac.uk>
2014-12-27 23:38:07 +0000

    Better calibration.

--- pi/Compass/Sensors.py
+++ pi/Compass/Sensors.py
@@ -20,7 +20,72 @@ XLoBorg.Init()
 #        except:
 #            print "Whoops!"
 
+K = 0.98
+
+max_offsets = (-431, 819, 2000)
+min_offsets = (-850, 432, 1000)
+
+offset_calculations = 1
+
+offset_sums = (-618, 700.00, 1427.00)
+
+Vx, Vy, Vz = (-618, 700.00, 1427.00)
+
+last_B = (0.0, 0.0)
+
+def readSensor():
+
+    global Vx
+    global Vy
+    global Vz
+    global max_offsets
+    global min_offsets
+    global offset_calculations
+    global offset_sums
+
+    Mx, My, Mz = XLoBorg.ReadCompassRaw()
+    
+    recalculate_offsets = False
+    
+    if (Mx > max_offsets[0]):
+        max_offsets[0] = Mx
+        recalculate_offsets = True
+    if (My > max_offsets[1]):
+        max_offsets[1] = My
+        recalculate_offsets = True
+    if (Mz > max_offsets[2]):
+        max_offsets[2] = Mz
+        recalculate_offsets = True
+  
+    if (Mx < min_offsets[0]):
+        min_offsets[0] = Mx
+        recalculate_offsets = True
+    if (My < min_offsets[1]):
+        min_offsets[1] = My
+        recalculate_offsets = True
+    if (Mz < min_offsets[2]):
+        min_offsets[2] = Mz
+        recalculate_offsets = True
+  
+    if (recalculate_offsets):
+        offset_calculations += 1
+        Vx = (offset_sums[0] + (max_offsets[0] + min_offsets[0])/2)/offset_calculations
+        offset_sums[0] += (max_offsets[0] + min_offsets[0])/2
+        Vy = (offset_sums[1] + (max_offsets[1] + min_offsets[1])/2)/offset_calculations
+        offset_sums[1] += (max_offsets[1] + min_offsets[1])/2
+        Vz = (offset_sums[2] + (max_offsets[2] + min_offsets[2])/2)/offset_calculations
+        offset_sums[2] += (max_offsets[2] + min_offsets[2])/2
+
+    Mx -= Vx
+    My -= Vy
+    Mz -= Vz
+
+    return (Mx, My, Mz)
+
 def heading() :
+
+    global last_B
+
     g = 9.81
     
     Ax, Ay, Az = XLoBorg.ReadAccelerometer()
@@ -34,12 +99,8 @@ def heading() :
     sin_roll = math.sin(roll)
     cos_roll = math.cos(roll)
 
-    Mx, My, Mz = XLoBorg.ReadCompassRaw()
-    Vx, Vy, Vz = (-618, 700.00, 1427.00) # hard iron estimate
-    Mx -= Vx
-    My -= Vy
-    Mz -= Vz
-    
+    Mx, My, Mz = readSensor()
+
     By = ((My * cos_roll - Mz * sin_roll))    # Eq 19 y component
     Mz = ((My * sin_roll + Mz * cos_roll))    # Bpy*sin(Phi)+Bpz*cos(Phi)
     Az = ((Ay * sin_roll + Az * cos_roll))    # Eq 15 denominator
@@ -47,7 +108,7 @@ def heading() :
     pitch = math.atan2(-Ax, Az)
     pitch_d = 180*pitch/math.pi
 
-    print "{0:.4f} {1:.4f}".format(roll_d, pitch_d)
+   # print "{0:.4f} {1:.4f}".format(roll_d, pitch_d)
 
     if (pitch > math.pi/2) :
         pitch = (math.pi - pitch)
@@ -66,7 +127,13 @@ def heading() :
     
     nBx = Bx/math.sqrt(Bx**2+By**2)
     nBy = By/math.sqrt(Bx**2+By**2)
+
+    nBx = K*last_B[0] + (K-1)*nBx
+    nBy = K*last_B[1] + (K-1)*nBy
     
+    last_B[0] = nBx
+    last_B[1] = nBy
+
     # calculate current yaw = e-compass angle Psi
     bearing = math.atan2(-nBy, nBx)  # Eq 22
     yaw_d = 180*bearing/math.pi
